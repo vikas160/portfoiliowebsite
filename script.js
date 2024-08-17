c404b3f4fc540bf80b4f43705aac272660b5da7d
$sudo <48377341+vikas160@users.noreply.github.com>
2024-08-17 14:48:38 +0530

    script.js

--- script.js
+++ script.js
@@ -26,3 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
     handleScroll();
     window.addEventListener('scroll', handleScroll);
 });
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const navList = document.querySelector('.nav-list');
+    navList.classList.toggle('active');
+});
